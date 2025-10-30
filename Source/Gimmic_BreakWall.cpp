0c3f4fc728fe57a4e639634a05d91dc903ee0b95
nano-ui <kou0168na1121@outlook.jp>
2025-10-30 12:25:33 +0900

    a

--- Source/Gimmic_BreakWall.cpp
+++ Source/Gimmic_BreakWall.cpp
@@ -4,16 +4,29 @@
 Gimmic_BreakWall::Gimmic_BreakWall()
 {
 	class_name = "Gimmic_BreakWall";
+	model = ModelManager::Instance().Load("Data/Model/bilud/saku.mdl");
 }
 
 void Gimmic_BreakWall::OnTrigger(GameObject* objects)
 {
-
+    hp--;
+    if (hp <= 0.0f) delete model;
 }
 
 void Gimmic_BreakWall::Update(float elapsedTime)
 {
-
+    // ① 壁のAABB（軸平行境界ボックス）を求める
+    halfSize = { scale.x * 0.5f, scale.y * 0.5f, scale.z * 0.5f };
+    boxMin = {
+        position.x - halfSize.x,
+        position.y - halfSize.y,
+        position.z - halfSize.z
+    };
+    boxMax = {
+        position.x + halfSize.x,
+        position.y + halfSize.y,
+        position.z + halfSize.z
+    };
 }
 
 REGISTER_GAMEOBJECT(Gimmic_BreakWall);
