c94762334cdf25397cf6eb43b8f0948aff249405
nano-ui <kou0168na1121@outlook.jp>
2025-10-30 11:31:01 +0900

    壊れる壁Update関数編集

--- Gimmic_BreakWall.cpp
+++ Gimmic_BreakWall.cpp
@@ -6,14 +6,34 @@ Gimmic_BreakWall::Gimmic_BreakWall()
 	class_name = "Gimmic_BreakWall";
 }
 
+//攻撃が当たったときに呼ばれる
 void Gimmic_BreakWall::OnTrigger(GameObject* objects)
 {
-
+	if (objects->type == Type::Player && !isBroken && hp > 0.0f)
+	{
+		hp--;
+	}
+	if (hp <= 0.0f && !isBroken)
+	{
+		isBroken = true;
+		delete model;
+	}
 }
 
 void Gimmic_BreakWall::Update(float elapsedTime)
 {
-
+	//壁のAABB（軸平行境界ボックス）を求める
+	halfSize = { scale.x * 0.5f,scale.y * 0.5f,scale.z * 0.5f };
+	boxMin = {
+		position.x - halfSize.x,
+		position.y - halfSize.y,
+		position.z - halfSize.z
+	};
+	boxMax = {
+		position.x + halfSize.x,
+		position.y + halfSize.y,
+		position.z + halfSize.z
+	};
 }
 
 REGISTER_GAMEOBJECT(Gimmic_BreakWall);
---
+++ Source/Gimmic_BreakWall.cpp
@@ -0,0 +1,20 @@
+#include "Gimmic_BreakWall.h"
+#include "Factory.h"
+
+Gimmic_BreakWall::Gimmic_BreakWall()
+{
+	class_name = "Gimmic_BreakWall";
+}
+
+void Gimmic_BreakWall::OnTrigger(GameObject* objects)
+{
+
+}
+
+void Gimmic_BreakWall::Update(float elapsedTime)
+{
+
+}
+
+REGISTER_GAMEOBJECT(Gimmic_BreakWall);
+
