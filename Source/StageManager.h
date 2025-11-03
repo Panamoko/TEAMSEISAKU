4454c2382c73a212e437acdc6d0150e6a954811c
nano-ui <kou0168na1121@outlook.jp>
2025-11-03 15:46:12 +0900

    stagemanagerクラス追加

---
+++ Source/StageManager.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include "Stage.h"
+#include "System/ModelRenderer.h"
+#include "System/ShapeRenderer.h"
+
+class StageManager
+{
+public:
+	static StageManager& Instance()
+	{
+		static StageManager instance;
+		return instance;
+	}
+
+	//コピー禁止
+	StageManager(const StageManager&) = delete;
+	StageManager& operator = (const StageManager&) = delete;
+
+	//ステージを登録
+	void Add(std::shared_ptr<Stage> stage);
+
+	//更新・描画
+	void Update(float elapsedTime);
+	void Render(const RenderContext& rc, ModelRenderer* renderer);
+	void RenderDebugPrimitive(const RenderContext& rc, ShapeRenderer* renderer);
+
+
+
+private:
+	StageManager() = default;
+	~StageManager() = default;
+
+	//登録されたすべてのGameObjectへのポインタを保持
+	std::vector < std::shared_ptr<Stage>> stages;
+
+};
