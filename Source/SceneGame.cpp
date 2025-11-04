da6ba7ad218d38a9b85856d10a702f0531f72086
nano-ui <kou0168na1121@outlook.jp>
2025-11-04 16:41:58 +0900

    SceneGame編集
    リリース時にeditorを表示しないようにした

--- Source/SceneGame.cpp
+++ Source/SceneGame.cpp
@@ -185,8 +185,9 @@ void SceneGame::Render()
 	rc.view       = camera.GetView();
 	rc.projection = camera.GetProjection();
 
+#if 1
 	game_editor.render(objects, sprites2d, ModelManager::Instance().GetModels(), modelRenderer);
-
+#endif
 	// 3Dƒ‚ƒfƒ‹•`‰æ
 	{
 		StageManager::Instance().Render(rc, modelRenderer);
