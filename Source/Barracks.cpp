15dd39ea6c210cf84eeb643e75bb731e911d226a
nano-ui <kou0168na1121@outlook.jp>
2025-11-10 18:24:38 +0900

    Barrackクラス作成
    敵が自動でスポーンするようにしました

---
+++ Source/Barracks.cpp
@@ -0,0 +1,75 @@
+#include "Barracks.h"
+#include "GimmicManager.h"
+#include "CollisionManager.h"
+#include "EnemyManager.h"
+
+
+Barracks::Barracks()
+{
+	//基礎設定
+	hp = 10.0f;
+	spawn_interval = 5.0f;
+	spawn_timer = 0.0f;
+	current_enemy_count = 0;
+	spawn_positon.z += 3.0f;
+
+	//当たり判定の種類設定
+	collider = std::make_unique<OBB>();
+	collider->type = ColliderType::OBB;
+	collider->owner = this;
+	obb = static_cast<OBB*>(collider.get());
+
+	CollisionManager::Instance().AddObject(this);
+}
+
+//ギミック更新処理
+void Barracks::Update(float elapsedTime)
+{
+	if (hp <= 0.0f)
+	{
+		CollisionManager::Instance().Remove(this);
+		GimmicManager::Instance().Remove(this);
+		return;
+	}
+
+	spawn_timer += elapsedTime;
+
+	//敵のスポーン数チェックと経過時間チェック
+	if (current_enemy_count < max_enemy_count && spawn_timer >= spawn_interval)
+	{
+		spawn_timer = 0.0f;
+
+		enemys = std::make_shared<EnemySlime>();	//敵をスポーン
+		enemys->SetPosition(spawn_positon);			//位置設定
+		EnemyManager::Instance().Register(enemys);	//敵を登録
+		spawned_enemies.push_back(enemys);			//兵舎にも敵を登録
+		current_enemy_count++;						//敵の現在数を増やす
+	}
+
+	//兵舎が管理する敵の死体を整理
+	spawned_enemies.erase(
+		std::remove_if(
+			spawned_enemies.begin(),
+			spawned_enemies.end(),
+			[&](const std::shared_ptr<Enemy>& enemy)
+			{
+				return enemy->IsDestroyRequested();
+			}),
+		spawned_enemies.end()
+	);
+
+}
+
+//デバッグ表示
+void Barracks::RenderDebugPrimitive(const RenderContext& rc, ShapeRenderer* renderer)
+{
+
+}
+
+//衝突処理
+void Barracks::OnCollision(GameObject* object)
+{
+	if (object->type == Type::PlayerAttack)hp -= 2.5f;
+}
+
+REGISTER_GAMEOBJECT(Barracks);
