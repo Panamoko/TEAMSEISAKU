86688736f07f6d526eab48d198b935feec7140bb
nano-ui <kou0168na1121@outlook.jp>
2025-11-27 10:54:37 +0900

    タイトルの画像を変えた

--- Source/ChangeSceneSytem.h
+++ Source/ChangeSceneSytem.h
@@ -1,12 +1,27 @@
 #pragma once
 #include <string>
-
+#include <System/Sprite.h>
+#include <System/Input.h>
 
 class ChangeSceneSytem
 {
 public:
 
+	ChangeSceneSytem();
+
+	~ChangeSceneSytem();
+
+	// XVˆ—
+	void Update(float elapsedTime);
+
+	// •`‰æˆ—
+	void Render();
+
+	// GUI•`‰æ
+	void DrawGUI();
+
 private:
+	Sprite* sprite = nullptr;
+
 
 };
-
