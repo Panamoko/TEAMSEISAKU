9b003e2c45177f06e1ae55c05a25fe3c81734299
nano-ui <kou0168na1121@outlook.jp>
2025-12-04 11:31:25 +0900

    弾の攻撃タイプを変更できるようにした

--- Source/Cannon.cpp
+++ Source/Cannon.cpp
@@ -12,7 +12,7 @@ Cannon::Cannon()
     //基礎設定
 	hp = 50.0f;
 	attac_interval = 3.0f;
-	attac_territory = 10.0f;
+	attac_territory = 15.0f;
 	attac_timer = 0.0f;
 	power = 10.0f;
 	speed = 3.0f;
@@ -112,9 +112,9 @@ void Cannon::Update(float elapsedTime)
                 };
 
                 //ProjectileStraiteのインスタンスを生成
-                ProjectileStraite* stratite = new ProjectileStraite(&projectileManager);
+                ProjectileStraite* stratite = new ProjectileStraite(&projectileManager, "Data/Model/Sword/Sword.mdl", type = Type::EnemyAttack);
 
-                stratite->Launch(launch_direction, { launch_position.x,launch_position.y + 1.0f,launch_position.z });
+                stratite->Launch(launch_direction, { launch_position.x - 0.3f,launch_position.y + 1.0f,launch_position.z });
 
                 attac_timer = 0.0f;
             }
