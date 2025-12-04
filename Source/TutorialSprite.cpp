6b570e3a54030391bed49befe2c9c62f71d563a6
nano-ui <kou0168na1121@outlook.jp>
2025-12-04 12:19:15 +0900

    a

--- Source/TutorialSprite.cpp
+++ Source/TutorialSprite.cpp
@@ -1,1 +1,37 @@
 #include "TutorialSprite.h"
+#include "Collision.h"
+#include <System/Graphics.h>
+#include <System/Mouse.h>
+#include <System/Input.h>
+
+TutorialSprite::TutorialSprite()
+{
+	click_pos = { 0.0f,0.0f };
+	click_size = { 1280.0f,720.0f };
+	position = { 0.0f,0.0f };
+}
+
+void TutorialSprite::AddSprite(const Sprite& sprite_date)
+{
+	sprite.push_back(sprite_date);
+}
+
+void TutorialSprite::Update()
+{
+	Mouse& mouse = Input::Instance().GetMouse();
+
+	click_pos.x = static_cast<float>(mouse.GetPositionX());
+	click_pos.y = static_cast<float>(mouse.GetPositionY());
+
+	bool is_mouse_over_sprite = Collision::IntersectPosSquare(
+		click_pos,
+		{ position.x,position.y },
+		{ click_size.x ,click_size.y });
+
+
+
+}
+
+void TutorialSprite::Render()
+{
+}
--- Source/TutorialSprite.h
+++ Source/TutorialSprite.h
@@ -1,11 +1,19 @@
 #pragma once
 #include "GameSprite.h"
+
 class TutorialSprite :
     public GameSprite
 {
 public:
+    TutorialSprite();
+    void AddSprite(const Sprite& sprite_date);
+    void Update();
+    void Render();
 
 private:
     int current_index;//Œ»İ•\¦‚µ‚Ä‚¢‚é‰æ‘œ‚Ì”Ô†
+    DirectX::XMFLOAT2 click_pos;
+    DirectX::XMFLOAT2 click_size;
+
     std::vector<Sprite> sprite;
 };
